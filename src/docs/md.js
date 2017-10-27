563c1330b4a33053ce532aaf0d98779d1344e0c3
Frankie Bagnardi <f.bagnardi@gmail.com>
2017-10-26 19:46:32 -0700

    basic syntax highlighting for docs

--- src/docs/md.js
+++ src/docs/md.js
@@ -8,12 +8,74 @@
 import * as React from 'react';
 import marked from 'marked';
 import outdent from 'outdent';
-import highlight from 'highlight.js';
-import './highlightjs.css';
+
+const escape1 = str => str
+  .replace(/</g, '%%%lt%%%')
+  .replace(/>/g, '%%%gt%%%')
+  .replace(/&/g, '%%%amp%%%')
+  .replace(/"/g, '%%%quot%%%');
 
 marked.setOptions({
+  sanitize: false,
   highlight(code) {
-    return highlight.highlight('jsx', code).value;
+    let output = '';
+    let langle = 0;
+    let jsxbrackets = 0;
+    let isStr = null;
+    let isNum = false;
+    for (let i = 0; i < code.length; i += 1) {
+      const char = code[i];
+      if (!isStr && char === '<') {
+        langle += 1;
+        output += '<span class="high-jsx">';
+        output += escape1(char);
+      } else if (!isStr && langle && char === '>') {
+        langle -= 1;
+        if (jsxbrackets && langle === 0) {
+          jsxbrackets -= 1;
+          output += '</span>';
+        }
+        output += escape1(char);
+        output += '</span>';
+
+        // TODO: other quotes?
+      } else if (!isStr && langle && char === '{') {
+        jsxbrackets += 1;
+        output += escape1(char);
+        output += `<span class="high-reset">`;
+      } else if (!isStr && langle && char === '}') {
+        jsxbrackets -= 1;
+        output += '</span>';
+        output += escape1(char);
+      } else if (char === "'" || char === '"' || char === '`') {
+        if (isStr === char) {
+          isStr = null;
+          output += escape1(char);
+          output += '</span>';
+        } else if (!isStr) {
+          isStr = char;
+          output += '<span class="high-str">';
+          output += escape1(char);
+        } else {
+          output += escape1(char);
+        }
+      } else if (isNum) {
+        if (/\d/.test(char)) {
+          output += escape1(char);
+        } else {
+          isNum = false;
+          output += '</span>';
+          output += escape1(char);
+        }
+      } else if (/\d/.test(char)) {
+        isNum = true;
+        output += '<span class="high-num">';
+        output += escape1(char);
+      } else {
+        output += escape1(char);
+      }
+    }
+    return output;
   },
 });
 
@@ -21,7 +83,8 @@ export default function md(strings: Array<string>, ...values: Array<any>) {
   const markdown = outdent(strings, ...values)
     .replace(/\|{4}/g, '```')
     .replace(/\|{2}/g, '`');
-  const html = marked(markdown);
+  const html = marked(markdown)
+    .replace(/%%%(\w+)%%%/g, '&$1;');
 
   /* eslint-disable react/no-danger */
   return (
