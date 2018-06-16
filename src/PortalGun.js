bd07f2ca97dcd95ab0a7342cd39670827b8156ec
Frankie Bagnardi <f.bagnardi@gmail.com>
2018-06-15 20:52:30 -0700

    fixes PortalGun cleanup on Source unmount

--- src/PortalGun.js
+++ src/PortalGun.js
@@ -1,20 +1,27 @@
+/* eslint-disable react/prop-types */
 import * as React from 'react';
 
 export default class PortalGun extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.listeners = [];
-    this.element = null;
-
-    this.Source = ({ children }) => {
-      this.element = children;
-      this.listeners.forEach((l) => l(this.element));
-      return null;
-    };
+  constructor(gunProps) {
+    super(gunProps);
 
     const gun = this;
-    this.Dest = class PortalGunDest extends React.Component {
+    gun.listeners = [];
+    gun.element = null;
+
+    gun.Source = class PortalGunSource extends React.Component {
+      componentWillUnmount() {
+        gun.element = null;
+        gun.listeners.forEach(listener => listener());
+      }
+      render() {
+        gun.element = this.props.children;
+        gun.listeners.forEach(listener => listener());
+        return null;
+      }
+    };
+
+    gun.Dest = class PortalGunDest extends React.Component {
       constructor(props) {
         super(props);
         this.state = { element: gun.element };
@@ -22,7 +29,7 @@ export default class PortalGun extends React.Component {
       componentDidMount() {
         this.listener = () => {
           this.setState({ element: gun.element });
-        }
+        };
         gun.listeners.push(this.listener);
       }
 
@@ -33,7 +40,7 @@ export default class PortalGun extends React.Component {
       render() {
         return this.state.element || null;
       }
-    }
+    };
   }
   render() {
     return this.props.children(this.Source, this.Dest);
