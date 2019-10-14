9e558685b42c5f9f9d4ad8032497411387f477dc
Taher <taher.dasten@gmail.com>
2019-10-14 22:46:35 +0530

    added breakpoint test

---
+++ src/__tests__/Breakpoint-hooks.js
@@ -0,0 +1,30 @@
+import '@testing-library/jest-dom/extend-expect';
+import * as React from 'react';
+import '@babel/polyfill';
+import { render, fireEvent } from '@testing-library/react';
+import useBreakpoint from '../BreakpointHooks';
+
+const breakpoints = [
+  { name: 'small', maxWidth: 600 },
+];
+
+it(`works`, async () => {
+  function Test() {
+    const bp = useBreakpoint(breakpoints);
+
+    return (
+      <React.Fragment>
+        <p>{window.innerWidth}</p>
+        <p data-testid="info">Sm: {bp.isEq('small').toString()}</p>
+      </React.Fragment>
+    );
+  }
+
+  const { getByTestId } = render(<Test />);
+  const node = getByTestId('info');
+
+  window.innerWidth = 600;
+  fireEvent(window, new Event('resize'));
+  expect(node).toHaveTextContent("Sm: true");
+});
+
